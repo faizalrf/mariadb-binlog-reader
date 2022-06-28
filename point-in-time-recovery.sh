10f05f793da326bda69a3d26e7cdf1369ad4fa65
Faisal Saeed <faisal@mariadb.com>
2022-06-29 02:24:58 +0800

    Base working dynamic auto restore script

--- point-in-time-recovery.sh
+++ point-in-time-recovery.sh
@@ -1,82 +1,19 @@
 #!/bin/bash
 
-backup() {
-   echo "GTID to Search = ${GTID_To_Search}"
-   FILES=$(seq -f "${BinLogName}.%06g" ${FirstFile} ${LastFile})
-   tmpFile=/tmp/extract.log
-   gtidSummary=/tmp/GTID_summary.out
-   rm -f ${gtidSummary}
-   #nextBinLogIsTheOne=0
-   touch /tmp/point-in-time-restore.sh
-   chmod 500 /tmp/point-in-time-restore.sh
-   commandString="mariadb-binlog "
-
-   if [ ! -d ${BinLogPath} ]; then
-      echo "Invalid binlog path ${BinLogPath} !"
-      exit 0
-   fi
-
-   if [[ ${BinLogPath} == "" ]]; then
-      BinLogPath="./"
-   fi
-
-   if [[ ${BinLogPath: -1} != "/" ]]; then
-      BinLogPath="${BinLogPath}/"      
-   fi
-   
-   # Scan through all the files in the list
-   for file in ${FILES}
-   do
-      binlogFile=${BinLogPath}${file}
-      if [ ! -f ${binlogFile} ]; then
-         echo "${binlogFile} file not found!"
-         continue
-      fi
-
-      if [ ${nextBinLogIsTheOne} -eq 1 ]; then
-         commandString="${commandString} ${binlogFile}"
-         echo ${commandString} >> point-in-time-restore.sh
-         continue
-      fi
-
-      echo "Reading ${binlogFile}..."
-
-      readBinlog=${binlogFile}.tmp
-      mariadb-binlog ${binlogFile} > ${readBinlog}
-      ret=$?
-      if [[ ${ret} == 0 ]]; then
-         # Extract the transaction Position from the binary log
-         # TODO extract next GTID position as well for mariabackup to start restore
-         grep -n "GTID *[0-9]*-[0-9]*-[0-9]*" ${readBinlog} | grep "GTID ${GTID_To_Search}" -A 1 > ${gtidSummary}
-
-         # Stop searching any other binlog files and conclude the next file is the one
-         if [ $(cat ${gtidSummary} | wc -l) -eq 1 ]; then
-            nextBinLogIsTheOne=1
-            continue
-         else
-            GTID_To_Search=$(tail -1  ${gtidSummary} | grep -o "?*[0-9]*-[0-9]*-[0-9]*")
-         fi
- 
-         startPos=$(grep "GTID ${GTID_To_Search}" -B 10 ${readBinlog} | grep "# at " | tail -1 | grep -o -E '[0-9]+')
-         ret=$?
-         if [[ ${ret} == 0 ]]; then
-            commandString="mariadb-binlog ${binlogFile} --start-position=${startPos} | mariadb"
-            echo ${commandString} >> point-in-time-restore.sh
-            nextBinLogIsTheOne=1
-         fi
-         startPos=0
-         rm -f ${readBinlog}
-      fi
-   done
-
-   rm -f ${readBinlog}
-
-   if [[ ${startPos} == 0 ]]; then 
-      echo "GTID ${GTID_To_Search} not found in the Binary Logs provided!"
-      exit 0
-   fi
-   echo "Binlog Startinbg position is ${startPos} and for the ${GTID_To_Search} within ${binlogFile}"
-}
+# Global variables 
+# cat xtrabackup_binlog_info | awk -F"-" '{print $NF}'
+# cat xtrabackup_binlog_info | grep -o "mariadb-bin.[0-9]*" | awk -F"mariadb-bin." '{print $NF}'
+binlogFile=""
+tmpFile=/tmp/extract.log
+gtidSummary=/tmp/GTID_summary.out
+gtidPosition=/tmp/GTID_position.out
+rm -f ${gtidSummary}
+binlogWithGTID=""
+nextBinLogIsTheOne=0
+restoreScript=/tmp/restore.sh
+echo "# Restore Script" > ${restoreScript}
+chmod 777 ${restoreScript}
+commandString="mariadb-binlog "
 
 ListGTID() {
    # Scan through all the files in the list
@@ -85,34 +22,62 @@ ListGTID() {
    do
       binlogFile=${BinLogPath}${file}
       # If Confirmed that next binary log is the one to be scanned from start then break this loop and continue
-
+      echo "GRID to search ${GTID_To_Search}"
       if [ ${nextBinLogIsTheOne} -gt 0 ]; then
          # Generate new Sequence and contunue 
          FirstFile=${fileNumber}
-         FILES=$(seq -f "${BinLogName}.%06g" ${FirstFile} ${LastFile})
+         FILES=$(ls -nrt ${BinLogPath}${BinLogName}.*[0-9]* | awk -F '.' '{if ($2 >= '"$FirstFile"') print $0}' | awk -F '.' '{if ($2 <= '"$LastFile"') print $0}' | grep -o "${BinLogName}.*[0-9]*")
          break
       fi
       if [ ! -f ${binlogFile} ]; then
          echo "${binlogFile} file not found!"
          continue
       fi
-      echo "Reading ${binlogFile}..."
+      echo "Reading ${binlogFile}"
 
       mariadb-binlog ${binlogFile} | grep -n "GTID *[0-9]*-[0-9]*-[0-9]*" | grep "${GTID_To_Search}" -A 1 > ${gtidSummary}
       ret=$?
       # If any problem reading the GTID summary, exit 1
       if [[ ${ret} == 0 ]]; then
+         cat ${gtidSummary}
          SummaryLines=$(cat ${gtidSummary} | wc -l)
          if [ ${SummaryLines} -eq 1 ]; then
             nextBinLogIsTheOne=1
+            echo "This is the last GTID in ${file}, next binlog will be applied to the DB from very first transaction..."
          elif [ ${SummaryLines} -eq 2 ]; then
             nextBinLogIsTheOne=2
             GTID_To_Search=$(tail -1  ${gtidSummary} | grep -o "?*[0-9]*-[0-9]*-[0-9]*")
+            binlogWithGTID=${file}
+            echo "The ${file} contains the GTID ${GTID_To_Search}, this file will be applied partially from the next GTID position..."
             continue
             # Contunue here because we have to scan the same file in the next function 
          fi
       fi
       ((fileNumber++))
+   done
+}
+
+FindGTIDPosition() {
+   for file in ${FILES}
+   do
+      binlogFile=${BinLogPath}${file}
+      if [[ ${nextBinLogIsTheOne} == 2 ]]; then
+         if [[ ${binlogWithGTID} == ${file} ]]; then
+            mariadb-binlog ${binlogFile} | grep "GTID ${GTID_To_Search}" -B 1 > ${gtidPosition}
+            startPos=$(grep "# at " ${gtidPosition} | head -1 | grep -o -E '[0-9]+')
+            ret=$?
+            if [[ ${ret} == 0 ]]; then
+               echo "Start position ${startPos}"
+               echo "mariadb-binlog ${binlogFile} --start-position=${startPos} | mariadb" >> ${restoreScript}
+               continue
+            else
+               echo "Error reading GTID position..."
+               echo
+               exit 1
+            fi
+         fi
+      fi
+      echo "mariadb-binlog ${binlogFile} | mariadb" >> ${restoreScript}
    done
 }
 
@@ -127,6 +92,39 @@ GenerateRestoreScript() {
       # Find the Position of this new GTID
       echo "Next Processing ${file}"
    done
+}
+
+GenerateFileList() {
+   if [[ ${FirstFile} > "0" ]]; then
+      echo "First File ${FirstFile}"
+   elif [ -d ${TargetDir} ]; then
+      if [ -f ${TargetDir}/xtrabackup_binlog_info ]; then
+         #Find the filenumber from the binlog file name in the xtrabackup_binlog_info
+         FirstFile=$(cat ${TargetDir}/xtrabackup_binlog_info | grep -o "${BinLogName}.[0-9]*" | awk -F"${BinLogName}." '{print $NF}')
+         FirstFile=$(expr ${FirstFile} + 0)
+         echo "First File ${FirstFile}"
+
+         GTID_To_Search=$(cat ${TargetDir}/xtrabackup_binlog_info | awk '{print $3}')
+      else
+         echo "Invalid backup directory path, `xtrabackup_binlog_info` file not found..."
+         exit 1
+      fi
+   fi
+   if [[ ${LastFile} > "0" ]]; then
+      echo "Last File ${LastFile}"
+      if [[ ${LastFile} < ${firstFile} ]]; then
+         echo "--end-file=${LastFile} must be higher than --start-file=${firstFile}, please check the arguments..."
+         echo "You may also want to validate the xtrabackup_binlog_info file for starting file position"
+         echo
+         exit 1
+      fi
+
+   else
+      LastFile=$(ls -nrt ${BinLogPath}${BinLogName}.[0-9]* | awk {'print $9'} | tail -1 | awk -F"${BinLogName}." '{print $NF}')
+      LastFile=$(expr ${LastFile} + 0)
+   fi
+
+   FILES=$(ls -nrt ${BinLogPath}${BinLogName}.*[0-9]* | awk -F '.' '{if ($2 >= '"$FirstFile"') print $0}' | awk -F '.' '{if ($2 <= '"$LastFile"') print $0}' | grep -o "${BinLogName}.*[0-9]*")
 }
 
 HomePath=$(pwd)
@@ -165,16 +163,6 @@ do
    esac
 done
 
-FILES=$(seq -f "${BinLogName}.%06g" ${FirstFile} ${LastFile})
-binlogFile=""
-tmpFile=/tmp/extract.log
-gtidSummary=/tmp/GTID_summary.out
-rm -f ${gtidSummary}
-nextBinLogIsTheOne=0
-touch /tmp/point-in-time-restore.sh
-chmod 500 /tmp/point-in-time-restore.sh
-commandString="mariadb-binlog "
-
 if [ ! -d ${BinLogPath} ]; then
    echo "Invalid binlog path ${BinLogPath} !"
    exit 0
@@ -188,12 +176,14 @@ if [[ ${BinLogPath: -1} != "/" ]]; then
    BinLogPath="${BinLogPath}/"      
 fi
 
-ListGTID
-
-if [ ! -f ${gtidSummary} ]; then
-   echo "Error processing, unable to open the GTID Summary ${gtidSummary}"
+if [[ ${TargetDir: -1} != "/" ]]; then
+   TargetDir="${TargetDir}/"      
 fi
 
-GenerateRestoreScript
+GenerateFileList
+
+ListGTID
+
+FindGTIDPosition
 
 exit 0
